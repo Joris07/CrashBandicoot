943884b109e2a7f16066a9dc1d88001d3b933553
ArthurJeannot <arthur.jeannot18@gmail.com>
2023-12-06 09:53:27 +0100

    Retry btn on death screen

--- CrashBandicoot/Assets/Scripts/GameController.cs
+++ CrashBandicoot/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -16,9 +17,19 @@ public class GameController : MonoBehaviour
 		timeAlive += Time.deltaTime;
 	}
 
-	public void OnDeath () {
+	public void OnDeath() {
 		panelDeath.SetActive(true);
-		timeAliveText.text = "Tu as tenu "+ Math.Round(timeAlive, 1) + " secondes";
-		Time.timeScale = .5f;
+		timeAliveText.text = "Tu as survécu "+ Math.Round(timeAlive, 1) + " secondes.";
+		Time.timeScale = 0.3f;
+	}
+
+	public void RestartBtn() {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("GameScene");
+	}	
+	
+	public void MenuBtn() {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("MenuScene");
 	}
 }
